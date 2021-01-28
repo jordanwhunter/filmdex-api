0179c0c812a806b9c8537a50b48ace3989258a74
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-01-28 16:23:44 -0500

    Connected to mongoDB Atlas

--- server.js
+++ server.js
@@ -1,4 +1,25 @@
-const express = require ('express')
-const app = express()
+require('dotenv').config();
 
-app.listen(3000, () => console.log('Server Has Started'))
+const express = require ('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true, useNewUrlParser: true }));
+app.use(express.json());
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    });
+    console.log('Connected To Your Mongo Database!')
+  } catch (error) {
+    console.log(error);
+  }
+}
+connectDB();
+
+app.listen(3000, () => console.log('Server Is Listening On Port 3000!'))
