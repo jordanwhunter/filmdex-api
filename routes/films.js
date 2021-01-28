dadab14d07e9ec02523ee1c221d077daf8d0fa52
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-01-28 18:01:00 -0500

    Psuedo-coded outline for CRUD operations

---
+++ routes/films.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const Film = require('../models/film');
+const router = express.Router();
+
+// Route for GETTING all film types
+
+// Route for GETTING one film type
+
+// Route for CREATING a new film type
+
+// Route for UPDATING an existing film type
+
+// Route for DELETING an existing film type
+
+module.exports = router
