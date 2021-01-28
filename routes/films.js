619755131e95ecdea9aebd1a23b59941ed0c680f
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-01-28 18:06:37 -0500

    Read operation for all

--- routes/films.js
+++ routes/films.js
@@ -3,6 +3,14 @@ const Film = require('../models/film');
 const router = express.Router();
 
 // Route for GETTING all film types
+router.get('/', async (req, res) => {
+  try {
+    const films = await Film.find()
+    res.json(films)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+})
 
 // Route for GETTING one film type
 
